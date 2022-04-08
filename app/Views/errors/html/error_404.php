17aaa40109b59ae11664c4007216fabb5cbab19d
shimozuki <robbishimozuki@gmail.com>
2022-04-08 16:17:05 +0800

    misterkong

--- app/Views/errors/html/error_404.php
+++ app/Views/errors/html/error_404.php
@@ -1,84 +1,66 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" >
 <head>
-	<meta charset="utf-8">
-	<title>404 Page Not Found</title>
-
-	<style>
-		div.logo {
-			height: 200px;
-			width: 155px;
-			display: inline-block;
-			opacity: 0.08;
-			position: absolute;
-			top: 2rem;
-			left: 50%;
-			margin-left: -73px;
-		}
-		body {
-			height: 100%;
-			background: #fafafa;
-			font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-			color: #777;
-			font-weight: 300;
-		}
-		h1 {
-			font-weight: lighter;
-			letter-spacing: 0.8;
-			font-size: 3rem;
-			margin-top: 0;
-			margin-bottom: 0;
-			color: #222;
-		}
-		.wrap {
-			max-width: 1024px;
-			margin: 5rem auto;
-			padding: 2rem;
-			background: #fff;
-			text-align: center;
-			border: 1px solid #efefef;
-			border-radius: 0.5rem;
-			position: relative;
-		}
-		pre {
-			white-space: normal;
-			margin-top: 1.5rem;
-		}
-		code {
-			background: #fafafa;
-			border: 1px solid #efefef;
-			padding: 0.5rem 1rem;
-			border-radius: 5px;
-			display: block;
-		}
-		p {
-			margin-top: 1.5rem;
-		}
-		.footer {
-			margin-top: 2rem;
-			border-top: 1px solid #efefef;
-			padding: 1em 2em 0 2em;
-			font-size: 85%;
-			color: #999;
-		}
-		a:active,
-		a:link,
-		a:visited {
-			color: #dd4814;
-		}
+  <meta charset="UTF-8">
+  <title>404 Page </title>
+  <script
+        src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+		<style type="text/css">
+		<?= preg_replace('#[\r\n\t ]+#', ' ', file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'style.css')) ?>
 	</style>
+
 </head>
 <body>
-	<div class="wrap">
-		<h1>404 - File Not Found</h1>
+<!-- partial:index.partial.html -->
+<!--
+VIEW IN FULL SCREEN MODE
+FULL SCREEN MODE: http://salehriaz.com/404Page/404.html
 
-		<p>
-			<?php if (! empty($message) && $message !== '(null)') : ?>
-				<?= nl2br(esc($message)) ?>
-			<?php else : ?>
-				Sorry! Cannot seem to find the page you were looking for.
-			<?php endif ?>
-		</p>
-	</div>
+DRIBBBLE: https://dribbble.com/shots/4330167-404-Page-Lost-In-Space
+-->
+
+<body class="bg-purple">
+        
+        <div class="stars">
+            <div class="custom-navbar">
+                <div class="brand-logo">
+                    <img src="https://misterkong.com/assets/landing_page/img/misterkong.svg" width="45%">
+                </div>
+            <div class="central-body">
+                <img class="image-404" src="http://salehriaz.com/404Page/img/404.svg" width="300px">
+				<p>
+				<?php if (! empty($message) && $message !== '(null)') : ?>
+				<h5><?= nl2br(esc($message)) ?></h5>
+				<?php else : ?>
+				<h5>Sorry! Cannot seem to find the page you were looking for.</h5>
+				<?php endif ?>
+				</p>
+                <a href="<?= base_url('/')?>" class="btn-go-home">GO BACK HOME</a>
+            </div>
+            <div class="objects">
+                <img class="object_rocket" src="http://salehriaz.com/404Page/img/rocket.svg" width="40px">
+                <div class="earth-moon">
+                    <img class="object_earth" src="http://salehriaz.com/404Page/img/earth.svg" width="100px">
+                    <img class="object_moon" src="http://salehriaz.com/404Page/img/moon.svg" width="80px">
+                </div>
+                <div class="box_astronaut">
+                    <img class="object_astronaut" src="http://salehriaz.com/404Page/img/astronaut.svg" width="140px">
+                </div>
+            </div>
+            <div class="glowing_stars">
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+                <div class="star"></div>
+
+            </div>
+
+        </div>
+
+    </body>
+<!-- partial -->
+  <script  src="script.js"></script>
+
 </body>
 </html>
